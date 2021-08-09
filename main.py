c326742ff4ba2918ad70f6a2727bc0141ffbf672
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-08-09 13:43:47 +0200

    feat: make the about and contact page

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
+blog_posts = requests.get("https://api.npoint.io/e42b353ee387383898c7").json()
 
 
 @app.route("/")
-def get_all_posts():
-    return render_template("index.html")
+def home():
+    return render_template("index.html", posts=blog_posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
